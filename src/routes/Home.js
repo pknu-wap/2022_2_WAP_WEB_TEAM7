f08755d291f1ef9056660a5196e0a542f9929a3c
jinha <75898490+iububble@users.noreply.github.com>
2022-11-09 18:37:50 +0900

    backend 연결 테스트 버튼 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "./Home.css";
+import axios from 'axios'
+function test() {
+    axios.post("http://192.168.0.38:8080/webKiosk/client/menu/read/",{
+        market_name:'S'}).then(response=>{console.log(response);
+        })
+    }
 
 function Home() {
     function handleClick(e) {
+        
         window.location.href = "/menu"
+
     }
     return (
         <div className="container">
             <button id="restaurant" onClick={handleClick}>매장</button>
             <button id="packaging" onClick={handleClick}>포장</button>
+            <button onClick={test}>test</button>
         </div>
     );
 }
