44145faa8cc1150610ea80fd1371d459fd2e4203
sgismine <113815235+sgismine@users.noreply.github.com>
2022-11-19 11:17:11 +0900

    Revert "Merge branch 'modified' of https://github.com/pknu-wap/2022_2_WAP_WEB_TEAM7 into modified"
    This reverts commit 5b92facad2dd68de731554d19e2c95b1895c90e3, reversing
    changes made to 05c058f702721f8b4fca86460d0abbe89c388f04.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,32 +1,30 @@
-import {React,useState,useEffect} from "react";
+import {React,useState} from "react";
 import "./Home.css";
 import axios from 'axios'
+function test() {
+    axios.post("http://211.220.33.201/webKiosk/client/menu/read/",{
+        market_name:'S'}).then(response=>{console.log(response);
+        })
+    }
 
 function Home() {
-    // 카테고리 목록을 담을 state 변수
-    const [category, set_category] = useState("");
-    // 메뉴 목로을 담을 state 변수
-    
     const [url, setUrl] = useState(null);
     function test() {
-        const data=axios.post("http://127.0.0.1:8000/webKiosk/client/option/read/",{
-            market_name : 'S'
-        })
-           .then(response=>{console.log(response.data)});
+        const data=axios.post("http://192.168.0.38:8080/webKiosk/client/menu/read/",{
+            market_name:'S'}).then(response=>{setUrl(response.data[0].menu_image)});
             }
 
     function handleClick(e) {
         
         window.location.href = "/menu"
-    }
 
+    }
     return (
         <div className="container">
             <button id="restaurant" onClick={handleClick}>매장</button>
             <button id="packaging" onClick={handleClick}>포장</button>
             <button onClick={test}>test</button>
-            <img src={"http://192.168.0.38:8000"+url}/>
-            <div>{JSON.stringify("")}</div>
+            <img src={"http://192.168.0.38:8080"+url}/>
         </div>
     );
 }
