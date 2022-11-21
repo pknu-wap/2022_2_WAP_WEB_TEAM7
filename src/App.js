e8add9e14650e0c7d978afb981f8c5622e14fdcc
jm730 <jaemyeong0730@naver.com>
2022-11-21 23:24:34 +0900

    오타 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Reactㅋ from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
 import Home from "./routes/Home";
