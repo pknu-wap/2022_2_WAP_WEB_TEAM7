84452fb246531ba141e2d90b918a4125340864a0
jm730 <jaemyeong0730@naver.com>
2022-11-25 15:39:04 +0900

    현재 주문목록 작업 중(임시)

--- src/layout/categories/Categories.js
+++ src/layout/categories/Categories.js
@@ -9,15 +9,15 @@ function Categories() {
     return (
         <div className="category_container">
             <ol>
-                <li className={isSelected === "Current" ? "selected" : "categories"} onClick={()=>{select('Current');}}>
-                    <Link to="/current_order">현재 주문</Link>
-                </li>
-                <li className={isSelected === "Past" ? "selected" : "categories"} onClick={()=>{select('Past');}}>
-                    <Link to="/past_order">지난 주문</Link>
-                </li>
-                <li className={isSelected === "Modify" ? "selected" : "categories"} onClick={()=>{select('Modify');}}>
-                    <Link to="/menu_modify">메뉴 수정</Link>
-                </li>
+                <Link to="/current_order">
+                     <li className={isSelected === "Current" ? "selected" : "categories"} onClick={()=>{select('Current');}}>현재 주문</li>
+                </Link>
+                <Link to="/past_order">
+                    <li className={isSelected === "Past" ? "selected" : "categories"} onClick={()=>{select('Past');}}>지난 주문</li>
+                </Link>
+                <Link to="/menu_modify">
+                    <li className={isSelected === "Modify" ? "selected" : "categories"} onClick={()=>{select('Modify');}}>메뉴 수정</li>
+                </Link>
             </ol>
         </div>
     )
