65d7358fb5d4bfdeea62ce470cc23841b1c5f081
jinha <75898490+iububble@users.noreply.github.com>
2022-11-29 02:17:29 +0900

    로그인 기능 임시로 구현
    현재 S를 제외한 다른 로그인 기능은 구현이 안됨

--- src/App.js
+++ src/App.js
@@ -11,8 +11,11 @@ import CurrentOrder from "./routes/CurrentOrder";
 import PastOrder from "./routes/PastOrder";
 import Menu from "./Menu/Menu";
 import Home from "./Menu/MenuEdit/Home.js";
+import { useCookies } from "react-cookie"; // useCookies import
 
 function App() {
+  const [cookies, setCookie, removeCookie] = useCookies(["id"]); // 쿠키 훅
+
   return (
     <Router>
       <div>
--- src/Menu/MenuEdit/Home.js
+++ src/Menu/MenuEdit/Home.js
@@ -12,7 +12,6 @@ const handleSubmit = (e) => {
 const Login = (props) => {
   const formRef = useRef();
   const [cookies, setCookie, removeCookie] = useCookies(["id"]); // 쿠키 훅
-  const [encodepw, setpw] = useState("");
   const navigate = useNavigate();
 
   const [show, setShow] = useState(false);
@@ -26,7 +25,7 @@ const Login = (props) => {
       .post("http://127.0.0.1:8000/webKiosk/account/login/", {
         // 로그인 요청
         userid: formRef.current.id.value,
-        password: test,
+        password: formRef.current.passWord.value,
         market_name: formRef.current.market_name.value,
       })
       .then((res) => {
