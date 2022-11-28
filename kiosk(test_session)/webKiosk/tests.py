4d19882dcc50b64b3bb73bde42469922a72f8596
DataExplorerXP <qw803qw803@gmail.com>
2022-11-28 22:13:53 +0900

    123

--- kiosk(test_session)/webKiosk/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-#from config.settings import *
-#print(STATICFILES_DIRS)
-password="aA123"
-print(password.islower()==True or password.isupper()==True)
