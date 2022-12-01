60977de06ffb34f93712301c00e7d39ce7c4c040
sgismine <113815235+sgismine@users.noreply.github.com>
2022-12-01 13:08:39 +0900

    홈화면 주문하기 버튼 + 로고이미지 첨부

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import {React,useState,useEffect} from "react";
 import "./Home.css";
 import axios from 'axios'
+import logo from '../image/logo8.PNG';
 
 function Home() {
     // 카테고리 목록을 담을 state 변수
@@ -14,19 +15,35 @@ function Home() {
         })
            .then(response=>{console.log(response.data)});
             }
+    
 
     function handleClick(e) {
-        
-        window.location.href = "/menu"
+        const eating = false;
+        window.location.href = "/menu";
     }
 
-    return (
+    /*function TakeoutClick(e) {
+        const eating = true;
+        window.location.href = "/menu";
+    }*/
+
+
+
+/*    return (
         <div className="container">
             <button id="restaurant" onClick={handleClick}>매장</button>
-            <button id="packaging" onClick={handleClick}>포장</button>
+            <button id="packaging" onClick={TakeoutClick}>포장</button>
             <button onClick={test}>test</button>
             <img src={"http://192.168.0.38:8000"+url}/>
             <div>{JSON.stringify("")}</div>
+        </div>
+    );
+}*/
+
+     return (
+        <div className="container">
+            <button id="restaurant" onClick={handleClick}>주문하기</button>
+            <img src = {logo} id ="kioskLogo" />
         </div>
     );
 }
